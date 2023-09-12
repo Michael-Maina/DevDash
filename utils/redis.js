a9751ef576b208f605cbe4890f19c3d67da86930
Dennis Murage <dmurage140@gmail.com>
2023-09-12 10:18:03 +0300

    This commit establishes the initial configuration for connecting to MongoDB and Redis databases. It includes the necessary connection parameters and initializes the connection pools for both databases

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (e) => { console.log(e.message); });
+
+    this.client.on('connect', () => {
+        console.log("Connected succesfully");
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, time) {
+    const asyncSet = promisify(this.client.set).bind(this.client);
+    await asyncSet(key, value, 'EX', time);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
