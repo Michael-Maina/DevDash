0f288eaf4830cb7d47374a791c72ebee413b92b1
Dennis Murage <dmurage140@gmail.com>
2023-09-20 15:04:08 +0300

    Fixing client start up of localhost:1234

--- web-terminal/client/test_app.js
+++ web-terminal/client/test_app.js
@@ -1,18 +1,23 @@
-// app.js
-import { fileURLToPath } from 'url';
-import path from 'path';
-import express from 'express';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
+const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'index.html');
-  res.sendFile(htmlFilePath);
+
+app.get('/tutorial', (req, res) => {
+  // Run Parcel to bundle the entry file
+  exec('npx parcel rebuild index.html', (error, stdout, stderr) => {
+    if (error) {
+      console.error('Parcel bundling error:', error);
+      return res.status(500).send('Error bundling JavaScript.');
+    }
+
+    // Serve the HTML file
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 });
 
 app.listen(port, () => {
