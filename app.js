b5d518662ed472653a6d666be7f6b08a9cdbe32c
Dennis Murage <dmurage140@gmail.com>
2023-09-27 12:13:32 +0300

    Merged abbie and michael branch

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRouter from './router/authRouter.js';
 import userRouter from './router/userRouter.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the current module's URL
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name
+const __dirname = dirname(__filename);
 
 // Load environment variables from .env file
 dotenv.config({ path: './.env' });
@@ -11,13 +19,20 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Serve static files from the "frontend" directory
+app.use(express.static("frontend"));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', authRouter);
 app.use('/:userId', userRouter);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to DevDash</h1>');
+  res.sendFile(__dirname + "/frontend/index.html");
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + "/frontend/about.html");
 });
 
 app.listen(PORT, () => {
--- router/authRouter.js
+++ router/authRouter.js
@@ -5,6 +5,12 @@ const router = express.Router();
 
 router.post("/signup", AuthController.signup);
 router.post("/login", AuthController.login);
+
+
+// router.get("/signup", (req, res) => {
+//   res.sendFile(__dirname + "/frontend/about.html");
+// })
+
 router.get('/login', (req, res) => {
   res.send('<h1>Login to DevDash</h1>');
 });
